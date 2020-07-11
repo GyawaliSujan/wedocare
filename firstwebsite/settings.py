8770972d37e4067cb1edad0da3f5f254c75dca32
GyawaliSujan <sujangyawali2010@gmail.com>
2020-07-11 19:26:23 +0545

    Adding aws settings

--- firstwebsite/settings.py
+++ firstwebsite/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -132,3 +133,12 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: 'danger',
 }
+#S3 BUCKETS CONFIG
+AWS_ACCESS_KEY_ID = 'AKIAY6DMGPYTB4IWBL5S'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'wedocare'
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
